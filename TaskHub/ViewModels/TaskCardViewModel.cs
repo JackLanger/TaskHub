c0f342f15955332c34f3a32ba4b7be6b44343ed4
JackLanger <jacek.l271186@gmail.com>
2020-10-05 16:14:19 +0200

    added new ViewModels for CardView

---
+++ TaskHub/ViewModels/TaskCardViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaskHub.ViewModels
+{
+    public class TaskCardViewModel:ModelBase
+    {
+
+        public string TaskName { get; set; }
+        public string TaskDescription { get; set; }
+        public DateTime DateAdded { get; set; }
+        public string PostedBy { get; set; }
+        public string TaskStatus { get; set; }
+        public bool IsActive { get; set; }
+        public bool IsSelected { get; set; }
+    }
+}
